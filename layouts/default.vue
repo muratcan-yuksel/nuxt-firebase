cf1831936a268dec33dc54e43ecbb2a2c7ebd917
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-08-27 20:34:01 +0300

    .

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app dark>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+
+    <script>
+      export default {};
+    </script>
+  </v-app></template
+>
