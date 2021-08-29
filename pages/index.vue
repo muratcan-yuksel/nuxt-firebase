302365950495974adbdda0ce06700b76dc986504
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-08-30 02:30:13 +0300

    styled just a bit

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,20 @@
 <template>
-  <div class="flex flex-column align-start align-self-start justify-start">
-    <h1>Welcome to your dashboard</h1>
-    <p>You are now logged in {{ $nuxt.$fire.auth.currentUser.email }}</p>
+  <div>
+    <div class="flex flex-column align-start align-self-start justify-start">
+      <h1>Let's watch some cats!</h1>
+      <p>You are now logged in {{ $nuxt.$fire.auth.currentUser.email }}</p>
+      <img
+        src="https://api.giphy.com/v1/gifs/translate?api_key=ShwQz1yuFlZwCalQ6bPu2O53jS1GC80E&s=cats"
+        id="images"
+        alt=""
+      />
+
+      <!-- <img :src="cats.data.images.original.url" alt="" /> -->
+    </div>
     <v-btn @click="$router.push('/auth/signout')">Logout</v-btn>
     <v-btn color="black" dark large @click="fetch">
       Another &nbsp; <v-icon>refresh</v-icon>
     </v-btn>
-    <!-- <img :src="cats.data.images.original.url" alt="" /> -->
-    <img src="https://api.giphy.com/v1/gifs/translate?api_key=ShwQz1yuFlZwCalQ6bPu2O53jS1GC80E&s=cats" id="images" alt="" />
-  
   </div>
 </template>
 
@@ -33,11 +39,7 @@ export default {
       document.getElementById("images").src = cats.data.images.original.url;
       return { cats };
     }
-  },
-
-  window.onload= fetch
-
-
+  }
 };
 </script>
 // f845d322-d851-40ee-9dcd-aac32e21ddbbs
