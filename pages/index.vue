6cdd4a0f1fc8ba83cfb0f1f1fb8df1aa359215d2
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-08-30 01:15:01 +0300

    console logs but image doesnt show

--- pages/index.vue
+++ pages/index.vue
@@ -3,11 +3,16 @@
     <h1>Welcome to your dashboard</h1>
     <p>You are now logged in {{ $nuxt.$fire.auth.currentUser.email }}</p>
     <v-btn @click="$router.push('/auth/signout')">Logout</v-btn>
-    <v-btn color="black" dark large @click="asyncData()">
+    <v-btn color="black" dark large @click="$fetch">
       Another &nbsp; <v-icon>refresh</v-icon>
     </v-btn>
-    <img :src="cats.data.images.original.url" alt="" />
-    <!-- <v-layout row wrap>
+    <!-- <img :src="cats.data.images.original.url" alt="" /> -->
+
+    <!-- 
+          <img :src="cats.data.images.original.url" alt="" />
+              <img :src="cats.data.images.original.url" alt="" />
+
+<v-layout row wrap>
       <v-flex xs12>
         <v-card flat tile class="d-flex">
           <v-img :src="image.url" contain> </v-img>
@@ -24,13 +29,30 @@ export default {
       cats: []
     };
   },
-  async asyncData({ $axios }) {
-    const cats = await $axios.$get(
+  activated() {
+    if (this.$fetchState.timestamp <= Date.now() - 30000) {
+      this.$fetch();
+    }
+  },
+
+  async fetch() {
+    const cats = await this.$axios.$get(
       "https://api.giphy.com/v1/gifs/translate?api_key=ShwQz1yuFlZwCalQ6bPu2O53jS1GC80E&s=cats"
     );
+
     console.log(cats.data.images.original.url);
     return { cats };
   }
+  // async asyncData({ $axios }) {
+  //   const cats = await $axios.$get(
+  //     "https://api.giphy.com/v1/gifs/translate?api_key=ShwQz1yuFlZwCalQ6bPu2O53jS1GC80E&s=cats"
+  //   );
+  //   console.log(cats.data.images.original.url);
+  //   const catsData = await response.json();
+  //   console.log(catsData.data.images.original.url);
+
+  //   return { catsData };
+  // }
 };
 </script>
 // f845d322-d851-40ee-9dcd-aac32e21ddbbs
